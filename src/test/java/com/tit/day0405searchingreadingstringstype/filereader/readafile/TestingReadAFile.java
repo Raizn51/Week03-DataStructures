02e5e3826fc9ba9cc7e2ac2db6d6e8787121e7e7
Raizn51 <Gouravraikwar989@gmail.com>
2025-02-06 10:23:09 +0530

    [Gourav Raikwar] Add . Program 01 of FileReader in which we are reading a large_text_file.txt file by using FileReader wrap in BufferedReader

---
+++ src/test/java/com/tit/day0405searchingreadingstringstype/filereader/readafile/TestingReadAFile.java
@@ -0,0 +1,20 @@
+package com.tit.day0405searchingreadingstringstype.filereader.readafile;
+
+import org.junit.jupiter.api.Test;
+
+// Creating a class TestingReadAFile to verify reading a file line by line
+public class TestingReadAFile
+{
+    @Test
+    public void testReadAFileLineByLine()
+    {
+        // Creating an instance of ReadAFiIe class
+        ReadAFiIe raf = new ReadAFiIe();
+        // Path to the test file
+        String filePath = "src/test/java/com/tit/day0405searchingreadingstringstype/filereader/readafile/large_text_file.txt";
+        // Calling the readAFileLineByLine method with the specified file path
+        raf.readAFileLineByLine(filePath);
+        // Printing a message to indicate the function is working as expected
+        System.out.println("readAFileLineByLine function working like expected");
+    }
+}
